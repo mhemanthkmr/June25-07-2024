b492a2fecd6ff8486d072e3a7d9ace28682ca978
mohamed abdul ajees <abdulajee2609@gmail.com>
2024-07-31 10:59:47 +0000

    update

--- wethw.py
+++ wethw.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.utilities import OpenWeatherMapAPIWrapper
-os.environ["OPENWEATHERMAP_API_KEY"]= api_key=("AI2_key")
+os.environ["OPENWEATHERMAP_API_KEY"]="AI2_key"
 weather = OpenWeatherMapAPIWrapper()
 location = input("enter the location : ")
 result=weather.run(location)
