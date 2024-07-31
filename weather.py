d858af1b8217e6c8e9275dd65681429248de1af7
HemanthKumar M <m.hemanthkmr143@gmail.com>
2024-07-31 05:52:42 +0000

    Code Modified

--- weather.py
+++ weather.py
@@ -1,8 +1,6 @@
 import os
-from langchain_community.utilities import OpenWeatherMapWrapper
-os.environ["OPEN_WEATHERMAP _API_KEY"]="a7708f9d88d41e367c132b1b500c37f9"
-weather = OpenWeatherMapWrapper(api_key=os.environ["OPEN_WEATHERMAP_API_KEY"])
+from langchain_community.utilities import OpenWeatherMapAPIWrapper
+os.environ["OPENWEATHERMAP_API_KEY"]="a7708f9d88d41e367c132b1b500c37f9"
+weather = OpenWeatherMapAPIWrapper()
 result=weather.run('ooty')
 print(result)
-
-N73LVTNQYUSIHJW3
