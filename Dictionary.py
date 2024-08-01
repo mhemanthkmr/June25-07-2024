ccbe8e89ac93872da98c11701089ad855ce1533f
Nithyasree19 <nithya19104@gmail.com>
2024-08-01 11:35:34 +0530

    file

---
+++ Dictionary.py
@@ -0,0 +1,11 @@
+#Task 1-- Dictionary#
+
+dict1={'name':'Nithya',
+'score':96}
+dict2={'name':'Fahima',
+'score':86}
+dict3={'name':'Vasima',
+'score':100}
+item=((f"{dict1['name']} scored {dict1['score']} marks"),(f"{dict2['name']} scored {dict2['score']} marks"),
+(f"{dict3['name']} scored {dict3['score']} marks"))
+print(item)
