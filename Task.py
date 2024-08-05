b2200345e86321f7b2343cee1f94e6c3876b3743
krithika <krithiprety2@gmail.com>
2024-08-05 11:36:15 +0530

    Update Task.py

---
+++ Task.py
@@ -0,0 +1,12 @@
+#task 1
+
+dict1={'name':'krithi',
+'score':96}
+dict2={'name':'bakiya',
+'score':86}
+dict3={'name':'sevi',
+'score':100}
+item=((f"{dict1['name']} scored {dict1['score']} marks"),(f"{dict2['name']} scored {dict2['score']} marks"),
+(f"{dict3['name']} scored {dict3['score']} marks"))
+print(item)
+
