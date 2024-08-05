740b4694bff309bfe51885d83cf9a7cdb4944cbe
krithika <krithiprety2@gmail.com>
2024-08-05 11:40:13 +0530

    Update Task4(2).py

--- Task4(2).py
+++ Task4(2).py
@@ -1,24 +1,12 @@
-#task4
-#list of cities per country
+#bmi
+weight=int(input())
+height=int(input())
+a=weight/height
+if a<35:
+    print("underweight")
+elif a>=40:
+    print("average")
+else :
+    print("obesity")
 
-india=["mumbai","bangalore","chennai","delhi"]
-pakistan=["lahore","karachi","islamabad"]
-bangladesh=["dhaka","khulna","rangpur"]
-print(india)
-print(pakistan)
-print(bangladesh)
-
-#a=str(input("Enter your city name:"))
-#b=str(input("Enter your country name:"))
-
-#print(a,b)
-
-x=input("Enter your city name:")
-y=input("Enter your city name:")
-
-if x in india :
-    if y in india:
-     print("both cities in india")
-else:
-    print("not in india")
 
