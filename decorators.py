02e550a3c3f25fba20656871328a9883c5f2f0ac
anu-priya-radha <anu84965@gmail.com>
2024-08-07 09:42:32 +0000

    decorators

---
+++ decorators.py
@@ -0,0 +1,35 @@
+import time
+import math
+
+def calculate_time(func):
+    def inner1(*args, **kwargs):
+        begin = time.time()
+        func(*args, **kwargs)
+        end = time.time()
+        print("Total time taken in :", func,__name__, end - begin)
+    return inner1
+
+def catchError(main):
+    def inner(*args, **kwargs):
+        try:
+            main(*args, **kwargs)
+        except Exception as err:
+            print(err)
+    return inner
+@calculate_time
+@catchError
+def divide(a,b):
+    time.sleep(2)
+    z=a/b
+    print(z)
+@catchError
+def add(a,b):
+    z=a+b
+    print(z)
+@catchError
+def sub(a,b):
+    z=a-b
+    print(z)
+divide(5,2)
+add(10,2)
+sub(8,2)
