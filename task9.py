da28915112d2c789fcf2854bffa99763de4c7d76
Sowbakiya subburaj <subbusowbakiya@gmail.com>
2024-08-12 19:44:00 +0530

    Update task9.py

--- task9.py
+++ task9.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def print_pattern(n):
     for i in range(1,n+1):
         for j in range(1,i+1):
@@ -6,12 +5,3 @@ def print_pattern(n):
         print()
 print_pattern(3)
 print_pattern(4)
-=======
-def print_pattern(n):
-    for i in range(1,n+1):
-        for j in range(1,i+1):
-            print(j,end='')
-        print()
-print_pattern(3)
-print_pattern(4)
->>>>>>> fbe0705c35c910957797a9bc4554fc01d6d103e9
